5de0b7f965e1890a50b82ade92c5c92527b3daef
Mr. Metelus <Mdmetelus@gmail.com>
2018-09-24 11:09:02 -0400

    Other Intro Work For lambda Preourse Practice.

---
+++ IntrotoWebDevHW/Lesson1Folder/notesAndInstructions.js
@@ -0,0 +1,39 @@
+/*
+Exercise I - Reflection
+Time: 20 minutes
+
+**********ALL ANSWERS TO THE QUESTIONS ARE IN THE HTML CODE, AND OUTPUT!!!!***********
+
+1) What does it mean to be a software engineer? Which of those qualities do you already have?
+
+2) What do you hope to get out of the Lambda School Mini Bootcamp?
+
+3) What are your long term programming goals?
+
+4) What parts of programming do you think you'll be good at?
+
+5) What part of programming do you think will be a challenge?
+
+6) Explain something that you didn't know about programming or web development before tonight.
+
+7) Pick a web app or web site you frequently use. What is an specific example of how that app or site uses HTML? CSS? Javascript? Example: If you picked lambdaschool.com you might say the "See How It Works" button text is an example of HTML. The red color of the button is an example of CSS. The way the color slightly changes when you hover the mouse is an example of JS.
+
+8) Using the same app or site from question 7, what were some challenges the programmers may have faced when developing it?
+
+
+Exercise II - Inspect a Website.
+Time: 20 minutes
+
+Pick a website you frequently use and open it the broswer. Right click anywhere on the page and select "Inspect". A side bar will appear that says "Elements". This is all the HTML that went into that specific webpage.
+
+1) Can you find a header, div, and p somewhere on the webpage?
+
+2) Find an HTML element you have not learned about yet. Explain what it is and how it's used. Use Google to help answer this question.
+
+3) If you double click an element you can edit it. Change the tag on a few different elements on the screen. What happens? Note: Some element cannot be edited, so try sticking with a div, p, or header if you're having trouble with this question.
+
+4) How does this exercise either support or change the ways you were thinking about HTML?
+
+Once you are done with these exercises, drag open the HTML screen to the left, and drag up the screen below this and complete Exercise III.
+
+*/
---
+++ IntrotoWebDevHW/Lesson2.1Folder/NandI.js
@@ -0,0 +1,30 @@
+/*
+Exercise I - Selector Quiz
+Time: 20 Minutes
+
+START HERE
+
+Answer the questions below based on the HTML / CSS to the left. Do not manipulate the HTML or CSS in any way until after you have completed the quiz (no peeking!). Afterwards you may delete line 1 from the CSS to check your work (reminder this is not graded! Try and compelte this without looking).
+
+1. Examine the h3 element containing "Step 1". What is the text color? What is the background color? Aqua blue
+
+
+2. Look at the h3 element containing "Step 3". What is the text and background color of this element?  black
+
+3. Look at all four paragraphs. What is the text color of each of them? p 1 is lightgray, p2 is lightgray, p3 is light gray, p 4 is orange because of final- step-text.
+
+4. What is the border color around the first div element? red
+
+5. What is the background color for the second div element?  There is no set background color on that div, it will be the default color white.
+
+6. What is the background color for each of the three p elements with the class "step-text"? the p1, p2 is blue. but the P3 is red because it is also in and Id tad that is lower that the class with a backqround of red.
+
+
+Stretch Questions:
+
+7. What is the text and background color of the h1 element? blue
+
+8. Of the p elements, which is the largest (takes up the most space on the page)?
+the first P is font size 20, the largest.
+
+*/
